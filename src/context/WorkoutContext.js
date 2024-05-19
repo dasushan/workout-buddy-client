ad5d8fd755a7dbc6bb75ba26127c81fa114251f9
dasushan <53913896+dasushan@users.noreply.github.com>
2024-05-19 16:20:15 +0530

    fixed eslint error

--- src/context/WorkoutContext.js
+++ src/context/WorkoutContext.js
@@ -17,7 +17,7 @@ export const workoutsReducer = (state, action) => {
     case 'DELETE_WORKOUT':
       return {
         workouts: state.workouts.filter(
-          (workout) => workout._id != action.payload._id
+          (workout) => workout._id !== action.payload._id
         ),
       };
     default:
